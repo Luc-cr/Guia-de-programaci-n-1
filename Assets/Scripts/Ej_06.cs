7b6cbf25f195e225d34cb0283176fdf7e26675c7
Luc-cr <72467699+Luc-cr@users.noreply.github.com>
2022-03-31 10:29:05 -0300

    Excercise 6 and 7

---
+++ Assets/Scripts/Ej_06.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*6. Realizá un programa que permita el ingreso por Inspector de un valor entero mayor que 0 en una variable llamada num1 y muestre 
+ * un mensaje por pantalla indicando "el  número es par" o "el número es impar". 
+ * Deberá utilizar el operador “módulo” es el caracter  %.*/
+
+public class Ej_06 : MonoBehaviour
+{
+    public int num1; 
+   
+    string checker(int num1)
+    {
+        bool esPar = num1 % 2 == 0;
+
+        if (esPar)
+        {
+            return "El numero es par";
+        }
+        else
+        {
+            return "El numero es impar";
+        }
+    }
+
+    void Start()
+    {
+        if (num1 <= 0)
+        {
+            Debug.Log("No se pueden ingresar numeros menores a o iguales 0");
+        }
+        else
+        {
+            Debug.Log(checker(num1));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Ej_07.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej_07 : MonoBehaviour
+{
+    public int numero;
+    public int numero2;
+    public int numero3;
+    string signo;
+
+    void Start()
+    {
+        if (numero > numero2 && numero > numero3) { signo = "Numero 1"; }
+        else if (numero2 > numero && numero2 > numero3) { signo = "Numero 2"; }
+        else if (numero3 > numero2 && numero3 > numero) { signo = "Numero 3"; }
+        else
+        {
+            signo = "todos iguales";
+        }
+
+        Debug.Log("El mayor numero es:" + signo);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
